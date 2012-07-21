170897cdcefe26f2ffe76ce7e5f121f762db5053
dbniceguy <dbniceguy@inervit.com>
2012-07-21 11:34:08 +0900

    Initial Commit

---
+++ logitec/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  logitec
+//
+//  Created by 순우 김 on 12. 7. 21..
+//  Copyright (c) 2012년 dbniceguy@inervit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ logitec/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  logitec
+//
+//  Created by 순우 김 on 12. 7. 21..
+//  Copyright (c) 2012년 dbniceguy@inervit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
