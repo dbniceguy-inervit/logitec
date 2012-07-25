6a80759d6997c8966d71998d7bdfda10e3ad3655
dbniceguy <dbniceguy@inervit.com>
2012-07-25 18:24:19 +0900

    카메라 추가 지도 추

--- logitec/interfaces/newOrEditDiaryViewController.h
+++ logitec/interfaces/newOrEditDiaryViewController.h
@@ -7,16 +7,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface newOrEditDiaryViewController : UIViewController
+@interface newOrEditDiaryViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+/*main scroll*/
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollViewMain;
+
+/*date*/
 @property (strong, nonatomic) IBOutlet UILabel *lbDate;
 - (IBAction)actionBtnDateChange:(id)sender;
+
+/*title*/
 @property (strong, nonatomic) IBOutlet UITextField *tfTitle;
+
+/*buttons*/
 - (IBAction)actionBtnWeather:(id)sender;
 - (IBAction)actionBtnLocation:(id)sender;
 - (IBAction)actionBtnPicture:(id)sender;
+
+/*Map view*/
+@property (strong, nonatomic) IBOutlet MKMapView *mvLocation;
+
+/*picture view*/
 @property (strong, nonatomic) IBOutlet UIImageView *ivPicture;
+@property (strong, nonatomic) UIImagePickerController *pcPicture;
+
+/*text view*/
 @property (strong, nonatomic) IBOutlet UITextView *tvDiary;
 
 @end
--- logitec/interfaces/newOrEditDiaryViewController.m
+++ logitec/interfaces/newOrEditDiaryViewController.m
@@ -13,7 +13,9 @@
 @end
 
 @implementation newOrEditDiaryViewController
+@synthesize mvLocation;
 @synthesize ivPicture;
+@synthesize pcPicture;
 @synthesize tvDiary;
 @synthesize tfTitle;
 @synthesize scrollViewMain;
@@ -42,6 +44,7 @@
     [self setTfTitle:nil];
     [self setIvPicture:nil];
     [self setTvDiary:nil];
+    [self setMvLocation:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -51,14 +54,34 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (IBAction)actionBtnDateChange:(id)sender {
+- (IBAction)actionBtnDateChange:(id)sender 
+{
 }
-- (IBAction)actionBtnWeather:(id)sender {
+- (IBAction)actionBtnWeather:(id)sender 
+{
 }
 
-- (IBAction)actionBtnLocation:(id)sender {
+- (IBAction)actionBtnLocation:(id)sender 
+{
 }
 
-- (IBAction)actionBtnPicture:(id)sender {
+- (IBAction)actionBtnPicture:(id)sender 
+{
+    pcPicture = [[UIImagePickerController alloc] init];
+	[pcPicture setDelegate:self];
+	[pcPicture setAllowsEditing:YES];
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        [pcPicture setSourceType:UIImagePickerControllerSourceTypeCamera];
+    }else{
+        [pcPicture setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    }
+	[self presentModalViewController:pcPicture animated:YES];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker 
+didFinishPickingMediaWithInfo:(NSDictionary *)info 
+{
+	ivPicture.image = [info objectForKey:UIImagePickerControllerEditedImage];
+	[picker dismissModalViewControllerAnimated:YES];
 }
 @end
