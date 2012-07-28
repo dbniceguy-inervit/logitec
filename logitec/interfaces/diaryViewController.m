0981e07ad21a9c44aa135f7732b86f880adf8f99
morenice <hyoungguyo@hotmail.com>
2012-07-28 12:49:55 +0900

    kim soon-woo. don't blame...

--- logitec/interfaces/diaryViewController.m
+++ logitec/interfaces/diaryViewController.m
@@ -5,7 +5,7 @@
 //  Created by 순우 김 on 12. 7. 21..
 //  Copyright (c) 2012년 dbniceguy@inervit.com. All rights reserved.
 //
-// Commit by Royal
+// Commit by morenice
 
 #import "diaryViewController.h"
 
